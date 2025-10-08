db5e2ed648a8994cdc0b81b3f1cdc2e260978ecc
GisDuck <sigaj04@mail.ru>
2025-10-08 20:06:28 +0300

    ff

---
+++ src/main/java/game/Happy_Zombie_Farm/HappyZombieFarmApplication.java
@@ -0,0 +1,13 @@
+package game.Happy_Zombie_Farm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyZombieFarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyZombieFarmApplication.class, args);
+	}
+
+}
