74783f581832af110a8869f075d5e22448e86266
GisDuck <sigaj04@mail.ru>
2025-10-08 20:06:28 +0300

    ff

---
+++ src/test/java/game/Happy_Zombie_Farm/HappyZombieFarmApplicationTests.java
@@ -0,0 +1,13 @@
+package game.Happy_Zombie_Farm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyZombieFarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
