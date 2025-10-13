941f3774d3d21a162c9b4c743b064023abfbc088
GisDuck <sigaj04@mail.ru>
2025-10-13 11:44:28 +0300

    ff

--- src/main/java/game/Happy_Zombie_Farm/HappyZombieFarmApplication.java
+++ src/main/java/game/Happy_Zombie_Farm/HappyZombieFarmApplication.java
@@ -3,7 +3,9 @@ package game.Happy_Zombie_Farm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+})
 public class HappyZombieFarmApplication {
 
 	public static void main(String[] args) {
