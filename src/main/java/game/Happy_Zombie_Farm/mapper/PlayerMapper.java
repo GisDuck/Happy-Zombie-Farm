848c183fee02a3b03d0bb9376b102d9e1c7c5b77
GisDuck <sigaj04@mail.ru>
2025-10-31 04:04:59 +0300

    Исправили маппер потому что вернули Лонг

--- src/main/java/game/Happy_Zombie_Farm/mapper/PlayerMapper.java
+++ src/main/java/game/Happy_Zombie_Farm/mapper/PlayerMapper.java
@@ -8,11 +8,5 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring", uses = {HouseMapper.class})
 public interface PlayerMapper {
 
-    @Mapping(target = "meat",
-            expression = "java(entity.getMeat() != null ? entity.getMeat().toString() : null)")
-    @Mapping(target = "gold",
-            expression = "java(entity.getGold() != null ? entity.getGold().toString() : null)")
-    @Mapping(target = "brain",
-            expression = "java(entity.getBrain() != null ? entity.getBrain().toString() : null)")
     PlayerDto toDto(Player entity);
 }
