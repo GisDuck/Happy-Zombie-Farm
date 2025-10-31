d3320a7861182389f415c51a50e55149f19dfd93
GisDuck <sigaj04@mail.ru>
2025-10-31 05:50:38 +0300

    копец

--- src/main/java/game/Happy_Zombie_Farm/HappyZombieFarmApplication.java
+++ src/main/java/game/Happy_Zombie_Farm/HappyZombieFarmApplication.java
@@ -1,11 +1,13 @@
 package game.Happy_Zombie_Farm;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+})
 @ConfigurationPropertiesScan("game.Happy_Zombie_Farm.config")
-@EnableConfigurationProperties
 public class HappyZombieFarmApplication {
 
 	public static void main(String[] args) {
