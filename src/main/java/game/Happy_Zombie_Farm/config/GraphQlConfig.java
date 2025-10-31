f25b1f94a9cd742de8cd0d439eaffab52775e366
GisDuck <sigaj04@mail.ru>
2025-10-31 03:30:00 +0300

    Добавили лонги и дата тайм

---
+++ src/main/java/game/Happy_Zombie_Farm/config/GraphQlConfig.java
@@ -0,0 +1,18 @@
+package game.Happy_Zombie_Farm.config;
+
+import graphql.scalars.ExtendedScalars;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.graphql.execution.RuntimeWiringConfigurer;
+
+@Configuration
+public class GraphQlConfig {
+
+    @Bean
+    public RuntimeWiringConfigurer runtimeWiringConfigurer() {
+        return wiringBuilder -> wiringBuilder
+                .scalar(ExtendedScalars.DateTime)
+                .scalar(ExtendedScalars.GraphQLLong)
+                .scalar(ExtendedScalars.Json);
+    }
+}
