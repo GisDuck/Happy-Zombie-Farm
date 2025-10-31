f839082392459c8a9fd4c4f4a72ab0b4f9e309e3
GisDuck <sigaj04@mail.ru>
2025-10-31 07:15:09 +0300

    бебебе 2

--- src/main/java/game/Happy_Zombie_Farm/controller/IndexController.java
+++ src/main/java/game/Happy_Zombie_Farm/controller/IndexController.java
@@ -9,16 +9,16 @@ public class IndexController {
     // корень сайта и /index → индекс
     @GetMapping({"/", "/index"})
     public String index() {
-        return "forward:/index.html";
+        return "forward:/public/index.html";
     }
 
-    // fallback для «левых» урлов типа /profile, /map и т.п.,
-    // НО мы не трогаем /api, /graphql, /auth и т.п.
-    @GetMapping({
-            "/{path:^(?!api|graphql|auth|actuator|ws).*$}",
-            "/{path:^(?!api|graphql|auth|actuator|ws).*$}/**"
-    })
-    public String spaFallback() {
-        return "forward:/index.html";
-    }
+//    // fallback для «левых» урлов типа /profile, /map и т.п.,
+//    // НО мы не трогаем /api, /graphql, /auth и т.п.
+//    @GetMapping({
+//            "/{path:^(?!api|public|graphql|auth|actuator|ws).*$}",
+//            "/{path:^(?!api|public|graphql|auth|actuator|ws).*$}/**"
+//    })
+//    public String spaFallback() {
+//        return "forward:/index.html";
+//    }
 }
