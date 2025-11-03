c8004e6ba5e40763d98e171a5f51af95a7e98909
GisDuck <sigaj04@mail.ru>
2025-11-03 14:27:25 +0300

    Постройка дома логи и тип enum

---
+++ src/main/java/game/Happy_Zombie_Farm/exception/WrongSkinHouseParamException.java
@@ -0,0 +1,16 @@
+package game.Happy_Zombie_Farm.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+@Getter
+public class WrongSkinHouseParamException extends RuntimeException {
+    private final String skin;
+
+    public WrongSkinHouseParamException(String type, String skin) {
+        super("Wrong skin house param skin=" + skin + "for type=" + type);
+        this.skin = skin;
+    }
+}
